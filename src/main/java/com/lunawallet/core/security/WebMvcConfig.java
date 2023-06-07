834a20f0cfb420d522cc3219b7357adf6284018c
italo luis <italoluis.221@gmail.com>
2023-06-06 21:15:55 -0300

    desenvolvimento da tela de login.

--- src/main/java/com/lunawallet/core/security/WebMvcConfig.java
+++ src/main/java/com/lunawallet/core/security/WebMvcConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,6 +20,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		registry.addViewController("/login").setViewName("pages/login");
 		registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
 		
+	}
+   
+   @Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	   registry.addResourceHandler("/**")
+       .addResourceLocations("classpath:/static/");
 	}
 
 }
